0b8aaf443c47399fda77555e90bdbe6f7dcdf40e
= <letmaxknow@gmail.com>
2022-09-13 20:15:46 -0400

    moved widget outside home-base.widget

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ Built for OSX (MacOS).
 
 1. Install and run [Übersicht](http://tracesof.net/uebersicht/).
 2. In the menu bar, under the Übersicht icon, click **Open Widgets Folder**.
-3. Move `/home-base.widget` into the Übersicht widgets folder.
+3. Move this folder into the Übersicht widgets folder.
 4. (Optional) In the menu bar, under the Übersicht icon, click **Preferences...** and check **Launch Übersicht when I login**.
--- index.jsx
+++ index.jsx
@@ -29,14 +29,14 @@ import { searchQuery, updateHistory, getSuggestions } from './src/search';
 const searchModes = [
   {
     mode: 'search',
-    iconSrc: 'home-base.widget/src/imgs/searchIcon.svg',
-    historySrc: 'home-base.widget/.searchHistory',
+    iconSrc: './home-base/home-base.widget/src/imgs/searchIcon.svg',
+    historySrc: './home-base/home-base.widget/.searchHistory',
     searchPlaceholder: 'Search ...'
   },
   {
     mode: 'web',
-    iconSrc: 'home-base.widget/src/imgs/webIcon.svg',
-    historySrc: 'home-base.widget/.webHistory',
+    iconSrc: './home-base/home-base.widget/src/imgs/webIcon.svg',
+    historySrc: './home-base/home-base.widget/.webHistory',
     searchPlaceholder: 'https:// ...'
   }
 ];
